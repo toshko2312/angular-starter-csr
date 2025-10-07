f54087579c60c8bfae5c818393cd62da33a2778a
Todor Milushev <toshko_2312@abv.bg>
2025-10-08 00:50:30 +0300

    feat(deployment): Add environments

--- src/app/core/configs/supabase.client.ts
+++ src/app/core/configs/supabase.client.ts
@@ -1,9 +1,8 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from '@environments/environment';
 
 export const supabase: SupabaseClient = createClient(
-  environment.SUPABASE_URL,
-  environment.SUPABASE_ANON_KEY, {
+  process.env['SUPABASE_URL']!,
+  process.env['SUPABASE_ANON_KEY']!, {
     auth: {
       persistSession: true,
       detectSessionInUrl: false,
